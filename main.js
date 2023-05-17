cff716ba764df9a0b352ef8f404a8f41b3dee8b6
Sibajit Maji <sibajitmaji2002@gmail.com>
2023-05-17 23:38:22 +0530

    index.htm added

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express=require("express")
 const app=express();
+const path = require('path');
+const port = process.env.port || 7000;
 
 app.get("/",(req,res)=>{
-    res.send("api call");
+    const filePath = path.join(__dirname,"index.htm");
+    res.sendFile(filePath);
 })
 
 app.listen(7000, ()=>{
-    console.log(`Server start at http://localhost:${7000}`);
+    console.log(`Server start at http://localhost:${7001}`);
 })
