3a74b0913595e0a86e6d577578ca5e612ceb4dd6
Sibajit Maji <sibajitmaji2002@gmail.com>
2023-05-18 10:03:19 +0530

    database connect

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express();
 const path = require('path');
-const ejs = require("ejs");
+const mongoose=require("mongoose");
 
 app.set('view engine','ejs');
 app.get("/",(req,res)=>{
@@ -11,6 +11,11 @@ app.get("/",(req,res)=>{
 app.get("/about",(req,res)=>{
     const filePath = path.join(__dirname,"home");
     res.render("home");
+})
+mongoose.connect("mongodb+srv://admin:admin@cluster0.o7ecu58.mongodb.net/aws?retryWrites=true&w=majority").then(()=>{
+    console.log("database connect");
+}).catch((err)=>{
+    console.log(err);
 })
 
 app.listen(7001, ()=>{
